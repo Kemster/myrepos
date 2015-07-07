417691aaf12349b9cacb32c9cd05f2a9fa1363ef
kemi <kemi@beamly.com>
2015-07-08 00:39:49 +0100

    version1

---
+++ python/article_page_load_test.py
@@ -0,0 +1,33 @@
+from browsermobproxy import Server
+import time
+from pprint import pprint
+import json
+import kemster_util as ku
+
+
+server = Server("./selenium/browsermob-proxy-2.1.0-beta-1/bin/browsermob-proxy")
+#server = Server("./selenium/browsermob-proxy-2.0-beta-6/bin/browsermob-proxy")
+server.start()
+proxy = server.create_proxy()
+
+from selenium import webdriver
+profile  = webdriver.FirefoxProfile()
+chrome = webdriver.chrome.options
+profile.set_proxy(proxy.selenium_proxy())
+driver = webdriver.Firefox(firefox_profile=profile)
+
+proxy.new_har("beamly GA test 1")
+driver.get("https://uk.beamly.com/tv-news/7-reasons-need-chloe-mad-fat-diary-best-friend/")
+
+
+driver.execute_script("window.scrollTo(0, 500);")
+time.sleep(5)
+
+resp = proxy.har # returns a HAR JSON blob
+proxy.close()
+server.stop()
+driver.quit()
+
+parsed_har = ku.parse_har_file(resp, 'google-analytics')
+pprint(parsed_har)
+json.dump(parsed_har, open('scenario1_test2.json', 'w+'))
